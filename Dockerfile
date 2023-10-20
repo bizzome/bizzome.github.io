8c4919367d6c0ef4b05f4263e63648b8ae8ec41d
bizzo <bizzo.mateus@gmail.com>
2023-10-20 16:32:02 -0300

    Revert adding nest js

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:16-alpine AS development
-WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install && npm ci --omit=dev
-COPY . .
-
-ENTRYPOINT ["npm", "run", "start"]
--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHomePage(): string {
-    return "<h1>Hi I'm Bizzo, a software developer!!</h1>";
-  }
-}
